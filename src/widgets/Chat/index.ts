713036a5c3536396742b7e7f7c9cf4fbfa93a794
Pellya <rpellya@gmail.com>
2025-04-04 23:47:16 +0300

    Remove unnecessery code

--- src/widgets/Chat/index.ts
+++ src/widgets/Chat/index.ts
@@ -1,4 +1,3 @@
-export { ChatList } from './ui/ChatList/ChatList';
 export { ChatCard } from './ui/ChatCard/ChatCard';
 export { ChatUser } from './ui/ChatUser/ChatUser';
 export type { Chat } from './model/types/schema';
--- src/widgets/Chat/ui/ChatList/ChatList.ts
+++
@@ -1,9 +0,0 @@
-import template from './ChatList.hbs';
-import Block from 'shared/lib/Block';
-import './ChatList.scss';
-
-export class ChatList extends Block {
-    render(): string {
-        return template;
-    }
-}
